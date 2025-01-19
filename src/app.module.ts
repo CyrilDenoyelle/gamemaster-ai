dbfd31f9b47d96f0ff5965858412e5b9cd75bf47
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2025-01-19 21:35:10 +0100

    feat: config module + discord bot

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DiscordModule } from './discord/discord.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'src/env/.env',
+      isGlobal: true,
+    }),
+    DiscordModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/discord/discord-client.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Client, GatewayIntentBits } from 'discord.js';
+
+@Injectable()
+export class DiscordClientService implements OnModuleInit, OnModuleDestroy {
+  private client: Client;
+
+  constructor() {
+    this.client = new Client({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildVoiceStates,
+      ],
+    });
+  }
+
+  async onModuleInit() {
+    console.log('Initializing Discord Bot...');
+    this.client.once('ready', () => {
+      console.log(`Logged in as ${this.client.user?.tag}`);
+    });
+
+    this.client.on('messageCreate', (message) => {
+      if (message.content === '!ping') {
+        message.reply('Pong!');
+      }
+    });
+    console.log('process.env.DISCORD_BOT_TOKEN', process.env.DISCORD_BOT_TOKEN);
+    await this.client.login(process.env.DISCORD_BOT_TOKEN);
+  }
+
+  async onModuleDestroy() {
+    console.log('Destroying Discord Bot...');
+    await this.client.destroy();
+  }
+}
