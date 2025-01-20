926700bb56a6ec14eadbadc5a7cb886bb85dd06f
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2025-01-20 19:40:24 +0100

    fix: stop double providing VoiceModule

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DiscordClientService } from './discord-client.service';
 import { MessageModule } from './message/message.module';
-import { VoiceService } from './voice/voice.service';
 import { VoiceModule } from './voice/voice.module';
 
 @Module({
   imports: [MessageModule, VoiceModule],
-  providers: [DiscordClientService, VoiceService],
+  providers: [DiscordClientService],
 })
 export class DiscordModule {}
