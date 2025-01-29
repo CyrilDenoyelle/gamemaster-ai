fd82899459a8aff8172c810bddf717e711b8a3ce
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2025-01-29 23:42:23 +0100

    feat: add system message and remove message to stay in given token limit

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { OpenAiService } from './open-ai/open-ai.service';
 import { Chat } from 'openai/resources';
-
+import { isWithinTokenLimit } from 'gpt-tokenizer';
 @Injectable()
 export class ChatService {
   constructor(private openAiService: OpenAiService) {}
 
-  chatHistory: Chat.ChatCompletionMessageParam[] = [
+  chatHistory: Chat.ChatCompletionMessageParam[] = [];
+  systemMessages: Chat.ChatCompletionMessageParam[] = [
     {
       role: 'system',
       content: 'You are a helpful assistant.',
@@ -23,7 +24,33 @@ export class ChatService {
     this.chatHistory.push({ role, content: text });
     console.log(`Sending text from ${userId}: ${text}`);
     if (role === 'user') {
-      this.openAiService.sendChat(this.chatHistory);
+      this.shiftMessagesUntilWithinLimit();
+      this.openAiService.sendChat([
+        ...this.systemMessages,
+        ...this.chatHistory,
+      ]);
     }
   }
+
+  /**
+   * shift messages until total tokens is less than process.env.OPENAI_API_MAX_CHAT_TOTAL_TOKEN
+   */
+  shiftMessagesUntilWithinLimit = () => {
+    const inlimite = () => {
+      const tokenCount = isWithinTokenLimit(
+        [...this.systemMessages, ...this.chatHistory]
+          .map((m) => `${m.role}: "${m.content}"\n`)
+          .join(''),
+        parseInt(process.env.OPENAI_API_MAX_CHAT_TOTAL_TOKEN, 10),
+      );
+      if (tokenCount) {
+        console.log('tokenCount', tokenCount);
+      }
+      return tokenCount;
+    };
+    while (inlimite() === false) {
+      console.log('shift');
+      this.chatHistory.shift();
+    }
+  };
 }
